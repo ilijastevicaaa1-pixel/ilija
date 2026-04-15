d29ba19b9b229a5f4eb72ce67f3ef7335f71fd19
Ilija <ilijastevicaaa1@gmail.com>
2026-04-15 11:14:07 +0200

    force deploy after root fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,8 @@ import App from './App';
 import './App.css';
 import { AuthProvider } from './auth/AuthContext.jsx';
 
+// force deploy
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
