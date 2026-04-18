6c20c74d7e051360c318820a0c920e918494a7d5
ilijastevicaaa1-pixel <ilijastevicaaa1@gmail.com>
2026-04-18 21:09:44 +0200

    final import-sql split ;

--- backend/import-sql.cjs
+++ backend/import-sql.cjs
@@ -2,38 +2,32 @@ const fs = require('fs');
 const { Client } = require('pg');
 
 async function importSQL() {
-    try {
-        const sql = fs.readFileSync('knjigovodstvo.sql', 'utf8');
+    const sql = fs.readFileSync('knjigovodstvo.sql', 'utf8');
 
-        // Better split for PostgreSQL dumps
-        const statements = sql
-            .split(/\r?\n\s*--|(?=CREATE|ALTER|INSERT|DROP|UPDATE)/i)
-            .map(s => s.trim())
-            .filter(s => s.length > 0);
+    const statements = sql
+        .split(/;\s*$/m)
+        .map(s => s.trim())
+        .filter(s => s.length > 0);
 
-        const client = new Client({
-            connectionString: process.env.DATABASE_URL,
-            ssl: { rejectUnauthorized: false }
-        });
+    const client = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }
+    });
 
-        await client.connect();
+    await client.connect();
 
-        for (let i = 0; i < statements.length; i++) {
-            const statement = statements[i];
-            try {
-                await client.query(statement);
-            } catch (stmtErr) {
-                console.log(`Statement ${i + 1} error:`, stmtErr.message);
-                continue;
-            }
+    for (const statement of statements) {
+        try {
+            await client.query(statement);
+        } catch (err) {
+            console.error("SQL error in statement:", statement);
+            throw err;
         }
-
-        await client.end();
-        return `SQL import završen (${statements.length} statements)`;
-    } catch (err) {
-        console.error('Import error:', err);
-        throw err;
     }
+
+    await client.end();
+
+    return "SQL import završen!";
 }
 
 module.exports = importSQL;
