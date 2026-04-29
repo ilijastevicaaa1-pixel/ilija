091a4ec4391950d61154eb4980a180e1f2303c67
ilijastevicaaa1-pixel <ilijastevicaaa1@gmail.com>
2026-04-29 06:57:27 +0200

    Add Rokovnik screen + SK translation + styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
+import Rokovnik from "./screens/Rokovnik";
 
 // Auth
 import LoginScreen from "./pages/LoginScreen";
