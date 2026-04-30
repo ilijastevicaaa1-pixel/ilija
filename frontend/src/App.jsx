6db38012c522a1ddf7459893ef73a68117542ecd
ilijastevicaaa1-pixel <ilijastevicaaa1@gmail.com>
2026-04-30 20:58:35 +0200

    add Rokovnik.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
-import Rokovnik from "./pages/Rokovnik.jsx";
+
 
 // Auth
 import LoginScreen from "./pages/LoginScreen";
